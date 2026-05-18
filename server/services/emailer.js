02f63ae25890260872dcdfb6b32ff52161d92a02
ankushkumarcity2001 <ankushkumar.city@gmail.com>
2026-05-18 16:53:24 +0530

    fix smtp port 587

--- server/services/emailer.js
+++ server/services/emailer.js
@@ -7,10 +7,11 @@
 import nodemailer from 'nodemailer';
 import fs from 'fs';
 import path from 'path';
-
 function createTransporter() {
   return nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
